9249f54ee3006d2f0bbe32785c949c0f3f04cf78
Arno VELETANLIC <git@arnov.dev>
2025-08-04 16:40:25 +0200

    bugfix: running command without subcommand does not error, prints minimal message to direct to --help

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import typer
 
-from browse.datasets import app as datasets
-from api.cds import app as cds
+from browse.commands import app as browse
+from api.commands import app as api
 
 cli = typer.Typer(
-    name="cds-data-fetcher",
-    help="CDS Data Fetcher",
+    name="copper",
+    help="Copernicus Data Fetcher & Browser",
     add_completion=True,
     rich_markup_mode="rich",
-    no_args_is_help=True,
 )
 
-cli.add_typer(datasets, name="datasets")
-cli.add_typer(cds, name="cds")
+cli.add_typer(browse, name="browse", help="Browse Copernicus datasets (data & metadata)")
+cli.add_typer(browse, name="br", hidden=True)
+cli.add_typer(api, name="api", help="Interact with Copernicus APIs and download data")
 
-
-@cli.callback()
+@cli.callback(invoke_without_command=True)
 def default():
-  pass
+  typer.echo("Use --help to see available commands")
+  raise typer.Exit(0)
 
 if __name__ == "__main__":
-    cli()
+  cli()
